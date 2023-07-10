65e84de03de0f986d7c95c96fa71a96ef567316f
yichen <1097179511@qq.com>
2023-07-10 13:43:09 +0800

    update
    update

--- test/main.c
+++ test/main.c
@@ -19,181 +19,216 @@ void gen_rand(BitSequence *rand_bits, int rand_bits_len)
     free(rand_bytes);
 }
 
+void save_bits_to_file(const char *filename, BitSequence *rand_bits, int rand_bits_len)
+{
+    FILE *file = fopen(filename, "wb");
+    if (file == NULL) {
+        printf("Failed to open file for writing.\n");
+        return;
+    }
+
+    fwrite(rand_bits, sizeof(BitSequence), rand_bits_len, file);
+    fclose(file);
+    printf("Bits saved to file: %s\n", filename);
+}
+
+BitSequence *read_bits_from_file(const char *filename, int rand_bits_len)
+{
+    BitSequence *rand_bits = (BitSequence *)calloc(rand_bits_len, sizeof(BitSequence));
+    if (rand_bits == NULL) {
+        printf("Failed to allocate memory.\n");
+        return NULL;
+    }
+
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        printf("Failed to open file for reading.\n");
+        free(rand_bits);
+        return NULL;
+    }
+
+    fread(rand_bits, sizeof(BitSequence), rand_bits_len, file);
+    fclose(file);
+    printf("Bits read from file: %s\n", filename);
+
+    return rand_bits;
+}
+
 int main(int argc, char **argv)
 {
     int ret = 0;
-    BitSequence *rand_bits = (BitSequence *)calloc(RAND_BIT_LEN, sizeof(BitSequence));
     clock_t begin = 0, end = 0;
+    const char *random_file = argv[1];
 
-    gen_rand(rand_bits, RAND_BIT_LEN);
+    BitSequence *bits_from_file = read_bits_from_file(random_file, RAND_BIT_LEN);
 
     //1
     begin = clock();
-    ret = Frequency(10000, rand_bits);
+    ret = Frequency(10000, bits_from_file);
     end = clock();
-    printf("--------------Frequency costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- Frequency costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===Frequency failed\n");
+        printf("[res]-------- Frequency failed\n");
     } else {
-        printf("Frequency pass\n");
+        printf("[res]-------- Frequency pass\n");
     }
 
     //2
     begin = clock();
-    ret = BlockFrequency(100, 10000, rand_bits);
+    ret = BlockFrequency(100, 10000, bits_from_file);
     end = clock();
-    printf("--------------BlockFrequency costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- BlockFrequency costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===BlockFrequency failed\n");
+        printf("[res]-------- BlockFrequency failed\n");
     } else {
-        printf("BlockFrequency pass\n");
+        printf("[res]-------- BlockFrequency pass\n");
     }
 
     //3
     begin = clock();
-    ret = PokerDetect(8, 10000, rand_bits);
+    ret = PokerDetect(8, 10000, bits_from_file);
     end = clock();
-    printf("--------------PokerDetect costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- PokerDetect costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===PokerDetect failed\n");
+        printf("[res]-------- PokerDetect failed\n");
     } else {
-        printf("PokerDetect pass\n");
+        printf("[res]-------- PokerDetect pass\n");
     }
 
     //4
     begin = clock();
-    ret = Serial(2, 10000, rand_bits);
+    ret = Serial(2, 10000, bits_from_file);
     end = clock();
-    printf("--------------Serial costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- Serial costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===Serial failed\n");
+        printf("[res]-------- Serial failed\n");
     } else {
-        printf("Serial pass\n");
+        printf("[res]-------- Serial pass\n");
     }
 
     //5
     begin = clock();
-    ret = Runs(1000000, rand_bits);
+    ret = Runs(1000000, bits_from_file);
     end = clock();
-    printf("--------------Runs costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- Runs costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===Runs failed\n");
+        printf("[res]-------- Runs failed\n");
     } else {
-        printf("Runs pass\n");
+        printf("[res]-------- Runs pass\n");
     }
 
     //6
     begin = clock();
-    ret = RunsDistribution(10000, rand_bits);
+    ret = RunsDistribution(10000, bits_from_file);
     end = clock();
-    printf("--------------RunsDistribution costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- RunsDistribution costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===RunsDistribution failed\n");
+        printf("[res]-------- RunsDistribution failed\n");
     } else {
-        printf("RunsDistribution pass\n");
+        printf("[res]-------- RunsDistribution pass\n");
     }
 
     //7
     begin = clock();
-    ret = LongestRunOfOnes(10000, rand_bits);
+    ret = LongestRunOfOnes(10000, bits_from_file);
     end = clock();
-    printf("--------------LongestRunOfOnes costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- LongestRunOfOnes costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===LongestRunOfOnes failed\n");
+        printf("[res]-------- LongestRunOfOnes failed\n");
     } else {
-        printf("LongestRunOfOnes pass\n");
+        printf("[res]-------- LongestRunOfOnes pass\n");
     }
 
     //8
     begin = clock();
-    ret = BinaryDerivate(3, 10000, rand_bits, sizeof(rand_bits));
+    ret = BinaryDerivate(3, 10000, bits_from_file, sizeof(bits_from_file));
     end = clock();
-    printf("--------------BinaryDerivate costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- BinaryDerivate costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===BinaryDerivate failed\n");
+        printf("[res]-------- BinaryDerivate failed\n");
     } else {
-        printf("BinaryDerivate pass\n");
+        printf("[res]-------- BinaryDerivate pass\n");
     }
 
     //9
     begin = clock();
-    ret = SelfCorrelation(8, 10000, rand_bits);
+    ret = SelfCorrelation(8, 10000, bits_from_file);
     end = clock();
-    printf("--------------SelfCorrelation costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- SelfCorrelation costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===SelfCorrelation failed\n");
+        printf("[res]-------- SelfCorrelation failed\n");
     } else {
-        printf("SelfCorrelation pass\n");
+        printf("[res]-------- SelfCorrelation pass\n");
     }
 
     //10
     begin = clock();
-    ret = Rank(10000, rand_bits);
+    ret = Rank(10000, bits_from_file);
     end = clock();
-    printf("--------------Rank costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- Rank costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===Rank failed\n");
+        printf("[res]-------- Rank failed\n");
     } else {
-        printf("Rank pass\n");
+        printf("[res]-------- Rank pass\n");
     }
 
     //11
     begin = clock();
-    ret = CumulativeSums(10000, rand_bits);
+    ret = CumulativeSums(10000, bits_from_file);
     end = clock();
-    printf("--------------CumulativeSums costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- CumulativeSums costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===CumulativeSums failed\n");
+        printf("[res]-------- CumulativeSums failed\n");
     } else {
-        printf("CumulativeSums pass\n");
+        printf("[res]-------- CumulativeSums pass\n");
     }
 
     //12
     begin = clock();
-    ret = ApproximateEntropy(5, 10000, rand_bits);
+    ret = ApproximateEntropy(5, 10000, bits_from_file);
     end = clock();
-    printf("--------------ApproximateEntropy costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- ApproximateEntropy costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===ApproximateEntropy failed\n");
+        printf("[res]-------- ApproximateEntropy failed\n");
     } else {
-        printf("ApproximateEntropy pass\n");
+        printf("[res]-------- ApproximateEntropy pass\n");
     }
 
     //13
     begin = clock();
-    ret = LinearComplexity(500, 1000000, rand_bits);
+    ret = LinearComplexity(500, 1000000, bits_from_file);
     end = clock();
-    printf("--------------LinearComplexity costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- LinearComplexity costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===LinearComplexity failed\n");
+        printf("[res]-------- LinearComplexity failed\n");
     } else {
-        printf("LinearComplexity pass\n");
+        printf("[res]-------- LinearComplexity pass\n");
     }
 
     //14
     begin = clock();
-    ret = Universal(1000000, rand_bits);
+    ret = Universal(1000000, bits_from_file);
     end = clock();
-    printf("--------------Universal costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- Universal costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===Universal failed\n");
+        printf("[res]-------- Universal failed\n");
     } else {
-        printf("Universal pass\n");
+        printf("[res]-------- Universal pass\n");
     }
 
     //15
     begin = clock();
-    ret = DiscreteFourierTransform(10000, rand_bits);
+    ret = DiscreteFourierTransform(10000, bits_from_file);
     end = clock();
-    printf("--------------DiscreteFourierTransform costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
+    printf("[log]-------- DiscreteFourierTransform costs %.3lf ms\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
     if (ret == 0) {
-        printf("!===DiscreteFourierTransform failed\n");
+        printf("[res]-------- DiscreteFourierTransform failed\n");
     } else {
-        printf("DiscreteFourierTransform pass\n");
+        printf("[res]-------- DiscreteFourierTransform pass\n");
     }
 
-    printf("all over!\n");
+    printf("\n");
 
-    free(rand_bits);
+    free(bits_from_file);
     return 0;
 }
